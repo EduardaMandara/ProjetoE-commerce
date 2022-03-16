0b0c574dc1508df3a357d1b90c51900765aefb30
xthiagosant <uzmaki_thiago@outlook.com>
2022-03-16 09:31:11 -0300

    removi o security

--- backend/src/main/java/com/tsswebapps/ecommerceapi/domain/model/Usuario.java
+++ backend/src/main/java/com/tsswebapps/ecommerceapi/domain/model/Usuario.java
@@ -1,7 +1,6 @@
 package com.tsswebapps.ecommerceapi.domain.model;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
@@ -15,13 +14,9 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 @Entity
 @Table
-public class Usuario implements UserDetails {
-	private static final long serialVersionUID = 1L;
+public class Usuario{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -103,41 +98,4 @@ public class Usuario implements UserDetails {
 				+ "]";
 	}
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return this.perfis;
-	}
-
-	@Override
-	public String getPassword() {
-		return this.senha;
-	}
-
-	@Override
-	public String getUsername() {
-		return this.email;
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
-	
-	
-	
 }
---
+++ backend/src/main/java/com/tsswebapps/ecommerceapi/infra/service/UserService.java
@@ -0,0 +1,5 @@
+package com.tsswebapps.ecommerceapi.infra.service;
+
+public class UserService {
+
+}
