6967e0ed012e40f65d35468b653b475ebdaf8f4b
xthiagosant <uzmaki_thiago@outlook.com>
2022-03-16 17:43:30 -0300

    ajustes na classe de seguranca para abrir geral

--- backend/src/main/java/com/tsswebapps/ecommerceapi/security/SecurityConfigurations.java
+++ backend/src/main/java/com/tsswebapps/ecommerceapi/security/SecurityConfigurations.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,13 +17,13 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers("/**").permitAll().and().cors();
+            http
+            .cors()
+            .and()
+            .csrf().disable();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-            auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
-    }
+
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
