ba47a4efad68388133a23c9bcf435b2fbcc98bf0
xthiagosant <uzmaki_thiago@outlook.com>
2022-03-16 10:50:21 -0300

    criado servico de login

--- backend/src/main/java/com/tsswebapps/ecommerceapi/domain/repository/UsuarioRepository.java
+++ backend/src/main/java/com/tsswebapps/ecommerceapi/domain/repository/UsuarioRepository.java
@@ -3,9 +3,12 @@ package com.tsswebapps.ecommerceapi.domain.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tsswebapps.ecommerceapi.domain.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	Optional<Usuario> findByEmail(String email);
+	Optional<Usuario> findByEmailAndSenha(String email, String senha);
 }
